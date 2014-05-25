73f0025c5adf84a1995dcd9998001beb19cb6525
Kirill Sibirev <l0kix2@gmail.com>
2014-05-25 22:29:07 +0400

    Add travis.yml

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 # coding: utf-8
 from setuptools import setup
+from pip.req import parse_requirements
+
+requirements = [
+    str(ir.req)
+    for ir in parse_requirements('requirements.txt')
+]
 
 setup(
     name='catcher',
     version='0.1.0',
+    install_requires=requirements,
 )
