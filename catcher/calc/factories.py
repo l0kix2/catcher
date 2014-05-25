057b66c631e9dfcb8ddaf0b8077bcff8750abb9e
Kirill Sibirev <l0kix2@gmail.com>
2014-05-25 22:41:35 +0400

    Go Travis

--- catcher/calc/factories.py
+++ catcher/calc/factories.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import six
+
 
 def counter(iterable):
     _counter = lambda accumulated, next_item: accumulated + 1
-    return reduce(_counter, iterable,  0)
+    return six.reduce(_counter, iterable,  0)
 
 
 def sum_(iterable, start=None):
